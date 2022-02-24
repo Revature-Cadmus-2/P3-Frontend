53a995d1a499b370bf8810aeec5fb86be923bd78
Michael Coffey <michaelcoffey1999@gmail.com>
2022-02-24 14:52:52 -0600

    cosmetic changes

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { ThemeService } from './service/theme.service';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,25 @@ export class AppComponent {
 
   constructor(public auth: AuthService, private themeService: ThemeService) { }
   
+  user: User = {
+    username: ''
+  };
+
+  ngOnInit(): void {
+    this.auth.user$.subscribe((userInfo)=> { 
+      this.user.username = userInfo.preferred_username;
+      if (userInfo?.preferred_username==null) {
+        console.log("nothing to show")
+      } else {
+        console.log(userInfo?.preferred_username);
+        console.log(userInfo);
+        sessionStorage.setItem('username', userInfo?.preferred_username);
+        const uN = sessionStorage.getItem('username');
+        console.log(uN+' this is my sessions storage var');
+      }
+    })
+  }
+  
   toggleDarkMode(){
     this.isDarkMode = this.themeService.isDarkMode();
     if (this.isDarkMode) {
@@ -20,5 +40,24 @@ export class AppComponent {
     } else {
       this.themeService.update('dark-mode');
     }
+  }
+
+  saveData(){
+    this.auth.user$.subscribe((userInfo)=> { 
+      if (userInfo?.username==null) {
+        console.log("nothing to show")
+      } else {
+        console.log(userInfo?.username);
+        console.log(userInfo);
+        sessionStorage.setItem('username', userInfo?.username);
+        
+      }
+    })
+  }
+
+  getData(){
+    return sessionStorage.getItem('username');
+    var username = sessionStorage.getItem('username');
+    console.log(username);
   }
 }
