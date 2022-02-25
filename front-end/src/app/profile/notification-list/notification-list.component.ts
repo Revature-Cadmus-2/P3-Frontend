2c068891adfdf1af6be9a70cfd41c3e22a55fdac
Michael Coffey <michaelcoffey1999@gmail.com>
2022-02-25 11:10:42 -0600

    notification-list fixes

--- front-end/src/app/profile/notification-list/notification-list.component.ts
+++ front-end/src/app/profile/notification-list/notification-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProfileService } from 'src/app/service/profile.service';
+import { Notification } from 'src/app/models/Notifications';
 
 @Component({
   selector: 'app-notification-list',
@@ -7,9 +10,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, public profileService: ProfileService, private router:Router) { }
 
   ngOnInit(): void {
+    
   }
 
 }
